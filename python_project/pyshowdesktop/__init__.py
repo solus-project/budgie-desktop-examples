cf5dfce1d04519bb16ee03a5953ad9135b76c9b8
Ikey Doherty <ikey@solus-project.com>
2016-05-05 21:18:57 +0100

    Throw in a dummy applet (incomplete)
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ python_project/pyshowdesktop/__init__.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python2.7
+
+# Copyright (C) 2016 Ikey Doherty
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#  
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import gi.repository
+gi.require_version('Budgie', '1.0')
+gi.require_version('Wnck', '3.0')
+from gi.repository import Budgie, GObject, Wnck
+
+class PyShowDesktop(GObject.GObject, Budgie.Plugin):
+    """ This is simply an entry point into your Budgie Applet implementation.
+        Note you must always override Object, and implement Plugin.
+    """
+
+    # Good manners, make sure we have unique name in GObject type system
+    __gtype_name__ = "PyShowDesktop"
+
+    def __init__(self):
+        """ Initialisation is important. This _must_ be Budgie.Plugin
+        """
+        Budgie.Plugin.__init__(self)
+
+    def do_get_panel_widget(self, uuid):
+        """ This is where the real fun happens. Return a new Budgie.Applet
+            instance with the given UUID. The UUID is determined by the
+            BudgiePanelManager, and is used for lifetime tracking.
+        """
+        return PyShowDesktopApplet(uuid)
+
+class PyShowDesktopApplet(Budgie.Applet):
+    """ Budgie.Applet is in fact a Gtk.Bin """
+
+    def __init__(self, uuid):
+        Budgie.Applet.__init__(self)
+        label = Gtk.Label("Hullo, world")
+
+        self.add(label)
+        self.show_all()
